e3be625cf77e3f379136877ab2541f5f55dcfa9e
Pedram Haqiqi <63091219+pedramhaqiqi@users.noreply.github.com>
2023-11-13 02:47:22 -0500

    update README & cleanup

--- lanpong/__main__.py
+++ lanpong/__main__.py
@@ -1,6 +1,6 @@
 """Entry point for lanpong."""
 
-from lanpong.cli import main  # pragma: no cover
+from lanpong.main import main  # pragma: no cover
 
 if __name__ == "__main__":  # pragma: no cover
     main()
--- lanpong/base.py
+++ lanpong/base.py
@@ -3,5 +3,4 @@ lanpong base module.
 
 """
 
-# example constant variable
 NAME = "lanpong"
