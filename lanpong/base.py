83db0dc6ff58684809507a4cfd50d179bfaa088a
Pedram Haqiqi <63091219+pedramhaqiqi@users.noreply.github.com>
2023-11-13 01:34:35 -0500

    Clean up

--- lanpong/base.py
+++ lanpong/base.py
@@ -1,16 +1,6 @@
 """
 lanpong base module.
 
-This is the principal module of the lanpong project.
-here you put your main classes and objects.
-
-Be creative! do whatever you want!
-
-If you want to replace this with a Flask application run:
-
-    $ make init
-
-and then choose `flask` as template.
 """
 
 # example constant variable
