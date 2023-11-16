e501317ae0ab496de2c1b30f787996fb9855088c
Pedram Haqiqi <63091219+pedramhaqiqi@users.noreply.github.com>
2023-11-16 01:04:11 -0500

    clean up

--- lanpong/server/server.py
+++ lanpong/server/server.py
@@ -55,6 +55,7 @@ class Server:
             print(f"Exception: {e}")
 
         finally:
+            self.connections.remove(client)
             channel.close()
 
     def start_server(self, host="0.0.0.0", port=2222):
@@ -65,23 +66,21 @@ class Server:
             port (int): Port. Defaults to 2222.
         """
 
-        # with socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        #  as server_sock:
-        server_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        server_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        server_sock.bind((host, port))
-        server_sock.listen(100)
-        print(f"Listening for connection on {host}:{port}")
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_sock:
+            server_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            server_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            server_sock.bind((host, port))
+            server_sock.listen(100)
+            print(f"Listening for connection on {host}:{port}")
 
-        # Accept multiple connections, thread-out
-        while True:
-            client_socket, client_addr = server_sock.accept()
-            # with client_socket as conn:
-            print(f"Incoming connection from {client_addr[0]}:{client_addr[1]}")
-            client_thread = threading.Thread(
-                target=self.handle_client, args=(client_socket,)
-            )
-            client_thread.start()
+            # Accept multiple connections, thread-out
+            while True:
+                client_socket, client_addr = server_sock.accept()
+                print(f"Incoming connection from {client_addr[0]}:{client_addr[1]}")
+                client_thread = threading.Thread(
+                    target=self.handle_client, args=(client_socket,)
+                )
+                client_thread.start()
 
     def __init__(self, key_file_name="test_key") -> None:
         self.server_key = paramiko.RSAKey.from_private_key_file(filename=key_file_name)
