e834997450e77d859c788658a914a52e95da1d0d
Pedram Haqiqi <63091219+pedramhaqiqi@users.noreply.github.com>
2023-12-10 02:08:08 -0500

    Finish ping build

--- lanpong/server/ping.py
+++ lanpong/server/ping.py
@@ -2,23 +2,30 @@ import subprocess
 import re
 
 
-def get_ping(ip_address):
-    command = ["ping", "-c", "1", ip_address]
-    process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    stdout, stderr = process.communicate()
-    if process.returncode == 0:
-        # Regex to extract the time from the output
-        match = re.search(r"time=(\d+\.?\d*)\s*ms", stdout.decode())
-        if match:
-            return float(match.group(1))
-    else:
-        print(f"Error pinging {ip_address}: {stderr}")
-        return None
+class Ping:
+    def __init__(self, ip) -> None:
+        self._cache = []
+        self.ip = ip
 
+    def get(self):
+        self.get_ping(self.ip)
+        average = sum(self._cache) / len(self._cache)
+        return round(average, 3)
 
-def ping_client(ip_address, player):
-    ping_time = get_ping(ip_address)
-    if ping_time is not None:
-        print(f"Ping time to player: {player} at {ip_address}: {ping_time} ms")
-    else:
-        print(f"Failed to get ping time for {ip_address}")
+    def get_ping(self, ip_address):
+        command = ["ping", "-c", "1", ip_address]
+        process = subprocess.Popen(
+            command, stdout=subprocess.PIPE, stderr=subprocess.PIPE
+        )
+        stdout, stderr = process.communicate()
+        if process.returncode == 0:
+            # Regex to extract the time from the output
+            match = re.search(r"time=(\d+\.?\d*)\s*ms", stdout.decode())
+            if match:
+                if len(self._cache) > 100:
+                    self._cache.pop(0)
+                self._cache.append(float(match.group(1)))
+
+        else:
+            print(f"Error pinging {ip_address}: {stderr}")
+            return None
