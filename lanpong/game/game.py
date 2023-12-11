ef710e1a32ee45c16a52d3208554b17a9b6aafcb
Sam Prokopchuk <ssprokopchuk@gmail.com>
2023-12-10 19:05:17 -0500

    Comments

--- lanpong/game/game.py
+++ lanpong/game/game.py
@@ -90,18 +90,28 @@ class Ball:
     def handle_paddle_collision(self, left_paddle, right_paddle):
         """
         Check if the ball has collided with a paddle.
-        :param left/right_paddle: The paddle to check collision with.
-        :return: True if the ball has collided with the paddle, False otherwise.
+
+        Args:
+            left_paddle (Paddle): The left paddle.
+            right_paddle (Paddle): The right paddle.
+
+        Returns:
+            None. Modifies the ball's velocity if a collision occurs.
         """
+        # Check if the ball's column position matches the right edge of the left paddle
+        # and if the ball's row position is within the vertical range of the left paddle
         if (
             left_paddle.col + 1 == self.get_col()
             and left_paddle.row <= self.get_row()
             and self.get_row() <= left_paddle.row + left_paddle.length - 1
         ) or (
+            # Check if the ball's column position matches the left edge of the right paddle
+            # and if the ball's row position is within the vertical range of the right paddle
             right_paddle.col - 1 == self.get_col()
             and right_paddle.row <= self.get_row()
             and self.get_row() <= right_paddle.row + right_paddle.length - 1
         ):
+            # If either condition is met, invert the ball's vertical velocity to simulate a bounce
             self.invert_col_velocity()
 
     def keep_within_bounds(self, rows, cols):
@@ -113,6 +123,7 @@ class Game:
     """
     Game object for pong
     """
+
     DEFAULT_ROWS = 36
     DEFAULT_COLS = 105
     STATS_HEIGHT = 5
@@ -131,7 +142,6 @@ class Game:
         self.score = [0, 0]
         self.score_timestamp = 0
         self.most_recent_score = -1
-
 
         self.is_game_started_event = threading.Event()
 
@@ -180,6 +190,7 @@ class Game:
         self._reset_ball()
 
     def draw_paddle(self, paddle):
+        """Draws a paddle on the screen"""
         self.screen[paddle.row : paddle.row + paddle.length, paddle.col] = b"|"
 
     def initialize_player(self):
@@ -201,8 +212,6 @@ class Game:
             self.player2 is not None and self.player2.is_ready
         ):
             self.is_game_started_event.set()
-        # else:
-        #     self.is_game_started_event.clear()
 
     def update_score(self, player_id):
         """Updates the score of the player"""
@@ -219,25 +228,46 @@ class Game:
             self.loser = 1
 
     def update_game(self):
+        """
+        Updates the game state.
+
+        This function handles the main logic for updating the game state, including ball movement,
+        collisions, score tracking, and screen updates.
+
+        Returns:
+            None. Modifies the internal state of the Game object.
+        """
+        # Check if the game is in the score display phase after a goal
         if time.time() - self.score_timestamp < self.SCORE_DISPLAY_TIME:
             return
+
+        # Reset the most recent score, indicating no recent score update
         self.most_recent_score = -1
+
+        # Check if the game is over
         if self.loser != 0:
-            # Game is over, don't update anything
+            # Game is over, don't update anything further
             return
-        # Erase the ball from the screen
+
+        # Erase the ball from its previous position on the screen
         self.screen[self.ball.get_row()][self.ball.get_col()] = b" "
 
+        # Update the ball's position based on its velocity
         self.ball.update_position()
 
+        # Check for collisions with the walls and update the score if a goal is scored
         score = self.ball.handle_wall_collision(self.nrows, self.ncols)
         if score != 0:
+            # Record the timestamp of the goal for score display
             self.score_timestamp = time.time()
+            # Update the most recent score and overall score
             self.most_recent_score = score
             self.update_score(score)
 
+        # Check for collisions with paddles and adjust ball velocity accordingly
         self.ball.handle_paddle_collision(self.player1.paddle, self.player2.paddle)
 
+        # Ensure the ball stays within the game boundaries
         self.ball.keep_within_bounds(self.nrows, self.ncols)
 
         # Update the ball position on the screen
@@ -253,19 +283,36 @@ class Game:
         return Game.screen_to_tui(screen)
 
     def update_paddle(self, player_number: int, key):
-        """Updates the paddle positions"""
+        """
+        Updates the paddle positions based on user input.
+
+        This function handles the paddle movement in response to user input.
+        It checks the validity of the input key and ensures that the paddle stays within the game boundaries.
+
+        Args:
+            player_number (int): The player number (1 or 2) whose paddle to update.
+            key (bytes): The user input key representing the desired paddle movement.
+
+        Returns:
+            None. Modifies the internal state of the Game object.
+        """
+        # Check if the game is in the score display phase after a goal
         if time.time() - self.score_timestamp < self.SCORE_DISPLAY_TIME:
             return
+
+        # Check if the game is over
         if self.loser != 0:
-            # Game is over, don't update anything
+            # Game is over, don't update anything further
             return
+
+        # Select the player and corresponding paddle based on the player number
         player = self.player1 if player_number == 1 else self.player2
         paddle = player.paddle
 
-        # Clear old paddle
+        # Clear the old paddle position on the screen
         self.screen[paddle.row : paddle.row + paddle.length, paddle.col] = b" "
 
-        # Only update paddle position if the key is valid and the paddle is not at the edge of the screen
+        # Check if the key is valid and update the paddle direction
         if key == b"w":
             paddle.direction = -1
         elif key == b"s":
@@ -273,13 +320,23 @@ class Game:
         elif key == b" ":
             paddle.direction = 0
 
+        # Update the paddle position based on the direction and ensure it stays within bounds
         if paddle.direction == -1 and paddle.row > 1:
             paddle.row -= 1
         elif paddle.direction == 1 and paddle.row < self.nrows - paddle.length - 1:
             paddle.row += 1
 
-        # Draw new paddle
+        # Draw the new paddle position on the screen
         self.draw_paddle(paddle)
+
+    def update_network_stats(self, stats, offset=1):
+        """Updates the network statistics area"""
+        self.screen[-self.STATS_HEIGHT + offset, 1:-2] = b" "
+        self.screen[-self.STATS_HEIGHT + offset, 1 : 1 + len(stats)] = list(stats)
+
+    def is_full(self):
+        """Returns True if the game is full, False otherwise"""
+        return self.player1 is not None and self.player2 is not None
 
     def __str__(self):
         if time.time() - self.score_timestamp < self.SCORE_DISPLAY_TIME:
@@ -288,9 +345,6 @@ class Game:
             )
 
         return Game.screen_to_tui(self.screen)
-
-    def is_full(self):
-        return self.player1 is not None and self.player2 is not None
 
     @staticmethod
     def get_blank_screen(
@@ -302,11 +356,6 @@ class Game:
         screen[0, :] = screen[-1, :] = screen[-stats_height - 1, :] = b"-"
         screen[:, 0] = screen[:, -1] = b"+"
         return screen
-
-    def update_network_stats(self, stats, offset=1):
-        """Updates the network statistics area"""
-        self.screen[-self.STATS_HEIGHT + offset, 1:-2] = b" "
-        self.screen[-self.STATS_HEIGHT + offset, 1 : 1 + len(stats)] = list(stats)
 
     @staticmethod
     def screen_to_tui(screen):
